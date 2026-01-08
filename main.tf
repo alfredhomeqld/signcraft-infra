897cf936e980a82df5b1d5e7deb18e80c19953c1
alfredhomeqld <michael@procloud.com.au>
2026-01-08 13:30:48 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,3 @@
-terraform {
-  required_providers {
-    hyperstack = {
-      source  = "registry.terraform.io/NexGenCloud/hyperstack"
-      version = "1.46.4-alpha" 
-    }
-    hcloud = {
-      source  = "registry.terraform.io/hetznercloud/hcloud"
-      version = "~> 1.45"
-    }
-  }
-}
-
-variable "hcloud_token" {
-  type      = string
-  sensitive = true
-}
-
-provider "hcloud" {
-  token = var.hcloud_token
-}
-
-provider "hyperstack" {}
-
-resource "hcloud_server" "main_server" {
-  name        = "SignCraft-Main-Helsinki"
-  server_type = "cx43"
-  image       = "ubuntu-22.04"
-  location    = "hel1"
-  public_net {
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-}
-
 resource "hyperstack_core_virtual_machine" "ai_vision" {
   name             = "signcraft-vision-l40"
   environment_name = "default-CANADA-1" 
@@ -42,8 +7,7 @@ resource "hyperstack_core_virtual_machine" "ai_vision" {
   assign_floating_ip = true
 
   lifecycle {
-    # This is the 'Nuclear Option' to stop the crash. 
-    # It tells Spacelift: "Just build it and leave it alone."
+    # This prevents the provider from crashing after creation
     ignore_changes = all
   }
 }
